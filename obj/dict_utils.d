3f465b8f44899d810080d1b335bc5cb1f86d90e9
Logan <contact@loganmartin.fr>
2025-12-14 17:48:42 +0100

    clean

--- obj/dict_utils.d
+++
@@ -1,3 +0,0 @@
-obj/dict_utils.o: src/dict_utils.c includes/minishell.h \
- libft/includes/libft.h libft/includes/exit.h libft/includes/ft_gc.h \
- libft/includes/get_next_line.h libft/includes/libft_structs.h
--- obj/parsing/envp_DEBUG.d
+++
@@ -1,3 +0,0 @@
-obj/parsing/envp_DEBUG.o: src/parsing/envp.c includes/minishell.h \
- libft/includes/libft.h libft/includes/exit.h libft/includes/ft_gc.h \
- libft/includes/get_next_line.h libft/includes/libft_structs.h
