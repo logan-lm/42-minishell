2ee75c7e93e1acb03753e9d2257e6e1d3660821f
bl4st <pberne@student.42.fr>
2025-12-29 14:18:04 +0100

    Var name increment

--- src/parsing/token_word_name.c
+++ src/parsing/token_word_name.c
@@ -6,7 +6,7 @@
 /*   By: pberne <pberne@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/27 09:52:58 by pberne            #+#    #+#             */
-/*   Updated: 2025/12/27 09:52:59 by pberne           ###   ########.fr       */
+/*   Updated: 2025/12/29 14:17:24 by pberne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,21 @@
 int	ft_is_name_character(char c)
 {
 	return (ft_isalnum(c) || c == '_');
+}
+
+int	ft_is_assignement_op(char *str, int *i)
+{
+	if (str[*i] == '=')
+	{
+		*i += 1;
+		return (1);
+	}
+	else if (str[*i] == '+' && str[*i + 1] == '=')
+	{
+		*i += 2;
+		return (1);
+	}
+	return (0);
 }
 
 t_string_compound_lst	*ft_build_word_token(char *str)
@@ -26,9 +41,8 @@ t_string_compound_lst	*ft_build_word_token(char *str)
 	i = 0;
 	while (str[i] && ft_is_name_character(str[i]))
 		i++;
-	if (i > 0 && str[i] == '=' && ft_isalpha(str[0]))
+	if (i > 0 && ft_isalpha(str[0]) && ft_is_assignement_op(str, &i))
 	{
-		i++;
 		ret = ft_get_string_token_node(str, i, word_true, 1);
 		j = i;
 		while (str[j])
