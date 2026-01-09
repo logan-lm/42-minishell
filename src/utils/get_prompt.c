bc41ef70547687fc5f14e879f73f7794bc45e915
pberne <pberne@student.42.fr>
2026-01-09 14:40:11 +0100

    fixed strlen issue on coloured prompt

--- src/utils/get_prompt.c
+++ src/utils/get_prompt.c
@@ -36,13 +36,13 @@ t_rgb	ft_get_color(int id)
 		return ((t_rgb){180, 255, 70});
 }
 
-char	*ft_get_char_colored(t_rgb color, char c)
+char	*ft_get_char_colored(t_rgb color, char *c)
 {
 	char	*str;
 
 	str = ft_strjoin_mult_gc_id(0, 9, "\001\033[38;2;", ft_itoa_gc(color.r),
 			";", ft_itoa_gc(color.g), ";", ft_itoa_gc(color.b), "m\002",
-			ft_substr_gc(&c, 0, 1), "\001\033[0m\002");
+			c, "\001\033[0m\002");
 	return (str);
 }
 
@@ -51,16 +51,16 @@ char	*ft_get_string_colored(int start_id)
 	char	*str;
 
 	str = ft_strjoin_mult_gc_id(0, 10,
-			ft_get_char_colored(ft_get_color((start_id) % 10), 'M'),
-			ft_get_char_colored(ft_get_color((start_id + 1) % 10), 'i'),
-			ft_get_char_colored(ft_get_color((start_id + 2) % 10), 'n'),
-			ft_get_char_colored(ft_get_color((start_id + 3) % 10), 'i'),
-			ft_get_char_colored(ft_get_color((start_id + 4) % 10), 's'),
-			ft_get_char_colored(ft_get_color((start_id + 5) % 10), 'h'),
-			ft_get_char_colored(ft_get_color((start_id + 6) % 10), 'e'),
-			ft_get_char_colored(ft_get_color((start_id + 7) % 10), 'l'),
-			ft_get_char_colored(ft_get_color((start_id + 8) % 10), 'l'),
-			ft_get_char_colored(ft_get_color((start_id + 9) % 10), '>'));
+			ft_get_char_colored(ft_get_color((start_id) % 10), "M"),
+			ft_get_char_colored(ft_get_color((start_id + 1) % 10), "i"),
+			ft_get_char_colored(ft_get_color((start_id + 2) % 10), "n"),
+			ft_get_char_colored(ft_get_color((start_id + 3) % 10), "i"),
+			ft_get_char_colored(ft_get_color((start_id + 4) % 10), "s"),
+			ft_get_char_colored(ft_get_color((start_id + 5) % 10), "h"),
+			ft_get_char_colored(ft_get_color((start_id + 6) % 10), "e"),
+			ft_get_char_colored(ft_get_color((start_id + 7) % 10), "l"),
+			ft_get_char_colored(ft_get_color((start_id + 8) % 10), "l"),
+			ft_get_char_colored(ft_get_color((start_id + 9) % 10), ">"));
 	return (str);
 }
 
