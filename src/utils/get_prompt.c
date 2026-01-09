ae5617e73332238cbac37bd6b8719ccb33aee5e9
pberne <pberne@student.42.fr>
2026-01-09 13:36:37 +0100

    Coloured prompt

--- src/utils/get_prompt.c
+++ src/utils/get_prompt.c
@@ -10,30 +10,83 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+t_rgb	ft_get_color(int id)
+{
+	if (id == 0)
+		return ((t_rgb){255, 239, 100});
+	if (id == 1)
+		return ((t_rgb){255, 196, 0});
+	if (id == 2)
+		return ((t_rgb){255, 161, 81});
+	if (id == 3)
+		return ((t_rgb){255, 122, 101});
+	if (id == 4)
+		return ((t_rgb){255, 104, 239});
+	if (id == 5)
+		return ((t_rgb){210, 120, 255});
+	if (id == 6)
+		return ((t_rgb){154, 160, 255});
+	if (id == 7)
+		return ((t_rgb){147, 237, 255});
+	if (id == 8)
+		return ((t_rgb){146, 255, 191});
+	else
+		return ((t_rgb){180, 255, 70});
+}
+
+char	*ft_get_char_colored(t_rgb color, char c)
+{
+	char	*str;
+
+	str = ft_strjoin_mult_gc_id(0, 9, "\001\033[38;2;", ft_itoa_gc(color.r),
+			";", ft_itoa_gc(color.g), ";", ft_itoa_gc(color.b), "m\002",
+			ft_substr_gc(&c, 0, 1), "\001\033[0m\002");
+	return (str);
+}
+
+char	*ft_get_string_colored(int start_id)
+{
+	char	*str;
+
+	str = ft_strjoin_mult_gc_id(0, 10,
+			ft_get_char_colored(ft_get_color((start_id) % 10), 'M'),
+			ft_get_char_colored(ft_get_color((start_id + 1) % 10), 'i'),
+			ft_get_char_colored(ft_get_color((start_id + 2) % 10), 'n'),
+			ft_get_char_colored(ft_get_color((start_id + 3) % 10), 'i'),
+			ft_get_char_colored(ft_get_color((start_id + 4) % 10), 's'),
+			ft_get_char_colored(ft_get_color((start_id + 5) % 10), 'h'),
+			ft_get_char_colored(ft_get_color((start_id + 6) % 10), 'e'),
+			ft_get_char_colored(ft_get_color((start_id + 7) % 10), 'l'),
+			ft_get_char_colored(ft_get_color((start_id + 8) % 10), 'l'),
+			ft_get_char_colored(ft_get_color((start_id + 9) % 10), '>'));
+	return (str);
+}
+
+char	**ft_setup_prompts(void)
+{
+	char	**p;
+	int		i;
+
+	p = ft_malloc(sizeof(char *) * 10);
+	i = 0;
+	while (i < 10)
+	{
+		p[i] = ft_get_string_colored(i);
+		i++;
+	}
+	return (p);
+}
+
 char	*ft_get_prompt(void)
 {
-	static int id = -1;
+	static int	id = -1;
+	static char	**prompts;
 
+	if (id == -1)
+		prompts = ft_setup_prompts();
 	id++;
-	id = id % 9;
-	char *str;
-	if (id == 0)
-		str = "\001\033[1;31m\002Minishell>\001\033[0m\002";
-	if (id == 1)
-		str = "\001\033[1;32m\002Minishell>\001\033[0m\002";
-	if (id == 2)
-		str = "\001\033[1;33m\002Minishell>\001\033[0m\002";
-	if (id == 3)
-		str = "\001\033[1;34m\002Minishell>\001\033[0m\002";
-	if (id == 4)
-		str = "\001\033[1;35m\002Minishell>\001\033[0m\002";
-	if (id == 5)
-		str = "\001\033[1;36m\002Minishell>\001\033[0m\002";
-	if (id == 6)
-		str = "\001\033[1;37m\002Minishell>\001\033[0m\002";
-	if (id == 7)
-		str = "\001\033[38;5;208m\002Minishell>\001\033[0m\002";
-	if (id == 8)
-		str = "\001\033[38;5;206m\002Minishell>\001\033[0m\002";
-	return (str);
+	id = id % 10;
+	return (prompts[id]);
 }
