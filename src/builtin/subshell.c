d813d68209e939481d5f71732a9866b5860979fc
Logan <contact@loganmartin.fr>
2026-01-10 20:24:11 +0100

    fix fd leak in subshells

--- src/builtin/subshell.c
+++ src/builtin/subshell.c
@@ -6,7 +6,7 @@
 /*   By: lomartin <lomartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 22:13:50 by lomartin          #+#    #+#             */
-/*   Updated: 2026/01/09 22:41:31 by lomartin         ###   ########.fr       */
+/*   Updated: 2026/01/10 20:22:08 by lomartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,15 @@ int	ft_subshell(char **args, t_shell_data *data, int fdin, int fdout)
 	if (pid == 0)
 	{
 		if (fdin != STDIN_FILENO)
+		{
 			dup2(fdin, STDIN_FILENO);
+			close(fdin);
+		}
 		if (fdout != STDOUT_FILENO)
+		{
 			dup2(fdout, STDOUT_FILENO);
+			close(fdout);
+		}
 		ret = ft_exec((t_command_node *)args[0], data);
 		ft_exit(ret);
 	}
